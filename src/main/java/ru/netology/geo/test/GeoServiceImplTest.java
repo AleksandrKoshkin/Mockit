b464652910c6d4a0b8553b20e79323b8b1c9b807
Aleksandr <aleksandr.koshkin.2000@mail.ru>
2023-05-18 19:12:25 +0300

    Three

---
+++ src/main/java/ru/netology/geo/test/GeoServiceImplTest.java
@@ -0,0 +1,17 @@
+package ru.netology.geo.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.netology.entity.Country;
+import ru.netology.entity.Location;
+import ru.netology.geo.GeoServiceImpl;
+
+public class GeoServiceImplTest {
+    @Test
+    public void GeoServiceImplTest() {
+        Location expected = new Location("Moscow", Country.RUSSIA, "Lenina", 15);
+        GeoServiceImpl geoService = new GeoServiceImpl();
+        Location result = geoService.byIp("172.0.32.11");
+        Assertions.assertEquals(expected.getCountry(), result.getCountry());
+    }
+}
---
+++ src/main/java/ru/netology/i18n/test/LocalizationServiceImplTest.java
@@ -0,0 +1,16 @@
+package ru.netology.i18n.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.netology.entity.Country;
+import ru.netology.i18n.LocalizationServiceImpl;
+
+public class LocalizationServiceImplTest {
+    @Test
+    public void LocalizationServiceImplTest() {
+        String expected = "Добро пожаловать";
+        LocalizationServiceImpl localizationService = new LocalizationServiceImpl();
+        String result = localizationService.locale(Country.RUSSIA);
+        Assertions.assertEquals(expected, result);
+    }
+}
